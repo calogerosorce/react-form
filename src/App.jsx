593bdf209048de9f384d4adb3ad25ff2d952c47e
calogerosorce <calogerosorce00@hotmail.com>
2025-11-10 13:14:47 +0100

    Add form, cicle map and function submit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 function App() {
 
-  const task = [{
+  const tasks = [{
     id: 1,
     title: 'Learn React'
   },
@@ -19,18 +19,28 @@ function App() {
     title: 'Celebrate!'
   }]
 
+  const [newTask, setNewTask] = useState('')
+  function handleSubmit(e) {
+    e.preventDefault()
+
+  }
 
   return (
     <>
       <div className="container  text-center">
         <h1>Todos List</h1>
         <div className="card p-4">
-          <div className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="New Title" aria-label="New Title" aria-describedby="add-title" />
-            <button className="btn btn-outline-secondary" type="submit" id="add-title">Add</button>
-          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" placeholder="New Title" aria-label="New Title" aria-describedby="add-title" value={newTask} onChange={(e) => setNewTask(e.target.value)} />
+              <button className="btn btn-outline-secondary" type="submit" id="add-title">Add</button>
+            </div>
+          </form>
           <ul className='list-group'>
-            <li className='list-group-item'>Title</li>
+            {tasks.map(items => (
+              <li key={items.id} className='list-group-item'>{items.title}</li>
+            )
+            )}
           </ul>
         </div>
       </div>
